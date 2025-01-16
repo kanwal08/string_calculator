d2e7dac9fbe2661174a58bfefc11d998c3d3bd46
Kanwal Singh <kpsingh0801@gmail.com>
2025-01-16 18:02:54 +0530

    Implementation for negative numbers

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,12 +1,21 @@
 class StringCalculator
- def self.add(numbers)
-  return 0 if numbers.empty?
-  
-  if numbers.start_with?("//")
-    delimeter, numbers = numbers[2..].split("\n", 2)
-    numbers.split(delimeter).map{|n| n.to_i}.sum
-  else
-  numbers.split(/[\n,]/).map{|n| n.to_i}.sum
+  def self.add(numbers)
+    return 0 if numbers.empty?
+
+      delimiter = /[\n,]/
+
+      if numbers.start_with?("//")
+        custom_delimiter, numbers = numbers[2..].split("\n", 2)
+        delimiter = Regexp.new(Regexp.escape(custom_delimiter.strip))
+      end
+
+      nums = numbers.split(delimiter).map(&:strip).map(&:to_i)
+
+      negative_numbers = nums.select { |n| n < 0 }
+      unless negative_numbers.empty?
+        raise "negative numbers not allowed: #{negative_numbers.join(', ')}"
+      end
+
+      nums.sum
   end
- end
 end
--- string_calculator_test.rb
+++ string_calculator_test.rb
@@ -19,9 +19,10 @@ assert_equal(15, StringCalculator.add("1\n2\n3\n4\n5"))
 assert_equal(15, StringCalculator.add("1,2\n3,4\n5"))
 assert_equal(210, StringCalculator.add("10,20\n30,40\n50,60"))
 # Test Case for delimeters"
-assert_equal(3, StringCalculator.add("//;\n 1; 2"))
-assert_equal(31, StringCalculator.add("//;\n1; 2;6;10;12"))
-assert_equal(10, StringCalculator.add("//|\n1| 2|3|4"))
+assert_equal(3, StringCalculator.add("//;\n1;2"))
+assert_equal(31, StringCalculator.add("//;\n1;2;6;10;12"))
+assert_equal(10, StringCalculator.add("//|\n1|2|3|4"))
+assert_equal(10, StringCalculator.add("//|\n1| 2|3 |4"))
 assert_equal(6, StringCalculator.add("//-\n1-2-3"))
 assert_equal(21, StringCalculator.add("//#\n1#2#3#4#5#6"))
 
