13896ac11a895252a266caaae4d3b9550e595ae3
Kanwal Singh <kpsingh0801@gmail.com>
2025-01-16 19:15:59 +0530

    Added documentation

--- string_calculator.rb
+++ string_calculator.rb
@@ -4,13 +4,16 @@ class StringCalculator
 
       delimiter = /[\n,]/
 
+      # Check for custom delimiter
       if numbers.start_with?("//")
         custom_delimiter, numbers = numbers[2..].split("\n", 2)
         delimiter = Regexp.new(Regexp.escape(custom_delimiter.strip))
       end
 
+      # Split numbers by delimiters and convert them to integers
       nums = numbers.split(delimiter).map(&:strip).map(&:to_i)
 
+      # Check for negative numbers
       negative_numbers = nums.select { |n| n < 0 }
       unless negative_numbers.empty?
         raise "negative numbers not allowed: #{negative_numbers.join(', ')}"
