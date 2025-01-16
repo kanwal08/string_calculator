7d24813c770e9c6c0b4b73259c61660a2d990907
Kanwal Singh <kpsingh0801@gmail.com>
2025-01-16 17:14:17 +0530

    Implementation for custom delimeters between numbers

--- string_calculator.rb
+++ string_calculator.rb
@@ -2,6 +2,11 @@ class StringCalculator
  def self.add(numbers)
   return 0 if numbers.empty?
   
+  if numbers.start_with?("//")
+    delimeter, numbers = numbers[2..].split("\n", 2)
+    numbers.split(delimeter).map{|n| n.to_i}.sum
+  else
   numbers.split(/[\n,]/).map{|n| n.to_i}.sum
+  end
  end
 end
